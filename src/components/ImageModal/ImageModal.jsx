3ff3ce9d935063c674eb7cfa098b950926d8ec2c
mtsiuniak <mtsuynyak1991@gmail.com>
2024-04-23 21:02:57 -0400

    changed styles

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -31,7 +31,6 @@ export default function ImageModal({
   return (
     <div>
       <Modal
-        className={css.container}
         isOpen={imgModal}
         onRequestClose={onModalClose}
         style={customStyles}
